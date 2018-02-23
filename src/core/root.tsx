8cc488f3f9d6ee954c7c9db887f6c858e11ec109
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-02-23 07:31:19 -0800

    Organize components

--- src/core/root.tsx
+++ src/core/root.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import Footer from "./footer";
-import Header from "./header";
-import Main from "./main";
+import Footer from "components/header";
+import Header from "components/footer";
+import Main from "core/main";
 
 const Root: React.SFC = () => (
   <div className="page-spread">
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Root from "./root";
+import Root from "core/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<Root />, document.getElementById("root"));
