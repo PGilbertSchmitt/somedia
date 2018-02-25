034087a718f218764aab24c7a35f22ce1ad07baa
PGilbertSchmitt <PGilbertSchmitt@gmail.com>
2018-02-24 18:37:01 -0800

    Add movement logic

--- src/components/selection/selection_grid.tsx
+++ src/components/selection/selection_grid.tsx
@@ -1,4 +1,7 @@
+import { merge, last } from "lodash";
 import React, { Component } from "react";
+
+import Selectable from "./selectable";
 
 /* SelectionGrid class for using arrow keys to navigate a grid of items and apply
  * special stylings to selected items
@@ -25,9 +28,101 @@ interface ISelectionGridProps {
   children: Component[];
 }
 
-class SelectionGrid extends Component<ISelectionGridProps, {}> {
+interface Coordinate {
+  row: number;
+  col: number;
+}
+
+type Row = Selectable[];
+
+type Grid = Row[];
+
+interface ISelectionGridState {
+  currentActive: Coordinate;
+  grid: Grid;
+}
+
+class SelectionGrid extends Component<ISelectionGridProps, ISelectionGridState> {
   constructor(props: ISelectionGridProps) {
     super(props);
+    this.setState({
+      currentActive: undefined
+    });
+
+    this.moveUp = this.moveUp.bind(this);
+    this.moveDown = this.moveDown.bind(this);
+    this.moveRight = this.moveRight.bind(this);
+    this.moveLeft = this.moveLeft.bind(this);
+  }
+
+  // This move logic can be cleaned up a bit, though many of the parts can be reused
+
+  coordinate(row, col: number): Coordinate {
+    return { row, col };
+  }
+
+  moveUp(): void {
+    let { grid, currentActive: co } = this.state;
+    let h = grid.length;
+
+    if (this.firstRow(co.row)) {
+      if (this.colInLastRow(grid, co)) {
+        co = this.coordinate(h - 1, co.col);
+      } else {
+        co = this.coordinate(h - 2, co.col);
+      }
+    } else {
+      co = this.coordinate(co.row - 1, co.col);
+    }
+
+    this.setState({ currentActive: co });
+  }
+
+  // This one is too difficult to do the if/else way, and looks cleaner. Might use
+  // this one for inspiration to clean up the others. Feels wrong somehow, though...
+  moveDown(): void {
+    let { grid, currentActive: co } = this.state;
+    let newCo = this.coordinate(co.row + 1, co.col);
+
+    if (!grid[newCo.row][newCo.col]) {
+      newCo.row = 0;
+    }
+
+    this.setState({ currentActive: newCo });
+  }
+
+  moveRight(): void {
+    let { grid, currentActive: co } = this.state;
+
+    if (this.lastInRow(grid, co)) {
+      if (this.lastRow(grid, co)) {
+        co = this.coordinate(0, 0);
+      } else {
+        co = this.coordinate(co.row + 1, 0);
+      }
+    } else {
+      co = this.coordinate(co.row, co.col + 1);
+    }
+
+    this.setState({ currentActive: co });
+  }
+
+  moveLeft(): void {
+    let { grid, currentActive: co } = this.state;
+    let w = this.props.gridWidth;
+    let h = grid.length;
+
+    if (this.firstInRow(co.col)) {
+      if (this.firstRow(co.row)) {
+        co = this.coordinate(h - 1, last(grid).length - 1);
+      } else {
+        co = this.coordinate(co.row - 1, w - 1);
+      }
+    } else {
+      co = this.coordinate(co.row, co.col - 1);
+    }
+
+    this.setState({ currentActive: co });
   }
 
   render() {
@@ -36,6 +131,26 @@ class SelectionGrid extends Component<ISelectionGridProps, {}> {
         grid stuff goes heres
       </div>
     );
+  }
+
+  private firstInRow(col: number): boolean {
+    return col === 0;
+  }
+
+  private firstRow(row: number): boolean {
+    return row === 0;
+  }
+
+  private lastInRow(grid: Grid, co: Coordinate): boolean {
+    return co.col === grid[co.row].length - 1;
+  }
+
+  private lastRow(grid: Grid, co: Coordinate): boolean {
+    return co.row === grid.length - 1;
+  }
+
+  private colInLastRow(grid: Grid, co: Coordinate): boolean {
+    return co.col < last(grid).length;
   }
 }
 
