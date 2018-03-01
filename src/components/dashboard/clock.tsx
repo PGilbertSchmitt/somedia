1661dd0740f72045cc6caa0409da634b05607d56
PGilbertSchmitt <PGilbertSchmitt@gmail.com>
2018-02-28 21:04:36 -0800

    Add time retrieval and format methods

--- src/components/dashboard/clock.tsx
+++ src/components/dashboard/clock.tsx
@@ -9,14 +9,24 @@ class Clock extends Component<{}, IClockState> {
     super(props);
     this.state = {
       currentTime: new Date()
-    }
+    };
 
     this.setCurrentTime = this.setCurrentTime.bind(this);
+  }
+
+  componentDidMount() {
+    // I could save the id, but I won't need to turn the clock off, so I won't bother
+    window.setTimeout(this.setCurrentTime, 60000);
   }
 
   setCurrentTime() {
     let currentTime = new Date();
     this.setState({ currentTime });
+  }
+
+  formatTime(time: Date): string {
+    let timeString = time.toLocaleTimeString();
+    return timeString.slice(0, 5);
   }
 
   render() {
